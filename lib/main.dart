bcaeff3a9ef01865919c8d7d3282a5434703fa5e
tejas-trivedi <500077185@stu.upes.ac.in>
2020-10-08 15:35:07 +0530

    stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,47 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
   }
 }
 
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
 
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'Whats your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 /*
 import 'package:flutter/material.dart';
