bf27c550b6bf6ed264c458966208cfac284e5061
tejas-trivedi <500077185@stu.upes.ac.in>
2020-10-10 00:07:42 +0530

    functionality done

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,14 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+    _questionIndex = 0;
+    _totalScore = 0; 
+    });
+
+  }
+
   void _answerQuestion(int score) {
     _totalScore += score;
 
@@ -75,7 +83,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(_totalScore),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
